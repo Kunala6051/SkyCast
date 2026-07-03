8c5368617e0a4a9bf435f3e4ec1f4439844672c6
Kunala6051 <kunalahuja74414@gmail.com>
2026-07-03 09:49:12 +0530

    Improve code readability

--- login.js
+++ login.js
@@ -2,23 +2,31 @@ const loginForm = document.getElementById("loginForm");
 const error = document.getElementById("error");
 
 loginForm.addEventListener("submit", async function (e) {
+
     e.preventDefault();
+
     error.textContent = "";
-    
+
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
+
     try {
+
         // Read credentials from users.json
         const response = await fetch("users.json");
+
         if (!response.ok) {
             throw new Error("Unable to load users.");
         }
+
         const users = await response.json();
+
         // Check username & password
         const validUser = users.find(user =>
             user.username === username &&
             user.password === password
         );
+
         if (validUser) {
             // Save logged in user
             localStorage.setItem("loggedInUser", validUser.username);
@@ -27,6 +35,7 @@ loginForm.addEventListener("submit", async function (e) {
         } else {
             error.textContent = "Invalid Username or Password.";
         }
+
     } catch (err) {
         error.textContent = "Something went wrong.";
         console.error(err);
